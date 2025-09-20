72fc98bf4698f5f80d1f0fd6fc9f45e34ba3b6fb
Shreyash-T02 <81151233+Shreyash-T02@users.noreply.github.com>
2025-09-20 18:56:13 +0000

    Deploying to gh-pages from @ Shreyash-T02/Shreyash-T02.github.io@a9804a5d9f6274fd60a3e5759ec066930bff9db4 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -459,14 +459,7 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
-        },
-      },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("mailto:%74%68%6F%72%61%74%73@%75%73%63.%65%64%75", "_blank");
         },
       },{
         id: 'social-rss',
