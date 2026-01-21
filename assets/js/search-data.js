a50edfcfb12af0bf06da7088bcd6801d13014822
Shreyash-T02 <81151233+Shreyash-T02@users.noreply.github.com>
2026-01-21 00:35:40 +0000

    Deploying to gh-pages from @ Shreyash-T02/Shreyash-T02.github.io@1bc553af7151356734fae8471fe722ee91e1d010 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -402,51 +402,11 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-package-handling-robot",
-          title: 'Package Handling Robot',
-          description: "Robotic manipulator with 3 kg payload and 700 mm reach for assembly line automation",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/1_project.html";
-            },},{id: "projects-package-handling-robotic-arm-for-pick-and-place-operations",
+          section: "News",},{id: "projects-package-handling-robotic-arm-for-pick-and-place-operations",
           title: 'Package Handling Robotic Arm for Pick and Place Operations',
-          description: "Design and control of 5 DOF Robotic Manipulator with electro magnetic end effector Passion Project | September 2024 – January 2025 | Mumbai, India",
+          description: "Design and control of 5 DOF Robotic Manipulator with electro magnetic end effector",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project.html";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/3_project.html";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/4_project.html";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project.html";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project.html";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project.html";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project.html";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project.html";
             },},{
         id: 'social-email',
         title: 'email',
